5110c1a8b657c101973bd4d6c1f557d6dedcb27a
Suhail M <suhailm2009@gmail.com>
2021-06-17 09:57:50 +0400

    Update cleardisplay.py

--- cleardisplay.py
+++ cleardisplay.py
@@ -1,11 +1,7 @@
 #!/usr/bin/python
-# -*- coding:utf-8 -*-
-
-import os
 
 import logging
 from lib.waveshare_epd import epd2in13_V2
-from PIL import Image,ImageDraw,ImageFont
 
 logging.basicConfig(level=logging.DEBUG)
 
@@ -13,17 +9,17 @@ try:
     logging.info("TASK: CLEAR DISPLAY")
     
     epd = epd2in13_V2.EPD()
-    logging.info("init and Clear")
+    logging.info("CLEARING DISPLAY")
     epd.init(epd.FULL_UPDATE)
     epd.Clear(0xFF)
     
-    logging.info("Go to Sleep, See Yaa..")
+    logging.info("Going to Sleep, See Yaa..")
     epd.sleep()
     
 except IOError as e:
     logging.info(e)
     
 except KeyboardInterrupt:    
-    logging.info("ctrl + c:")
+    logging.info("ctrl+c: FIRED")
     epd2in13_V2.epdconfig.module_exit()
     exit()
